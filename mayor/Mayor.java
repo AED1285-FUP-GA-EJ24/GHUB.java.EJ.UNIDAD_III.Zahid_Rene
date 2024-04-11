ead46ce5282e6d7ed9b37d9be8b30e02cd1fefe8
Zahid Rene Jimenez Olivas <159456299+REHID-35@users.noreply.github.com>
2024-04-11 15:03:17 +0000

    actualización 11/04/2024

---
+++ mayor/Mayor.java
@@ -0,0 +1,25 @@
+package mayor;
+
+import java.util.Scanner;
+
+public class Mayor {
+    public void mayor() {
+        Scanner teclado=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("Proporcionar nuemro de a,b,c");
+    a=teclado.nextInt();
+    b=teclado.nextInt();
+    c=teclado.nextInt();
+    if(a>b){
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+
+        }
+
+    }
+
+    }
+    
+}
