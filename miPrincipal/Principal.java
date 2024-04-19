a34da2f5706efc6cee10a67487d58d18bdf0007e
Zahid Rene Jimenez Olivas <159456299+REHID-35@users.noreply.github.com>
2024-04-19 15:03:58 +0000

    actualización 19/04/2024

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -5,6 +5,6 @@ package miPrincipal;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("");
     }
 }
